4999573d13fe1b4411285e4f75dc8670905e7c24
Matias Hermanrud Fjeld <mhf@hex.no>
2013-11-04 10:12:55 +0100

    Rettet felles php i sidene for salsa og løping #18 #5 #9

--- www/løping.php
+++ www/løping.php
@@ -1,12 +1,11 @@
 <?php
-
-require_once "../common.php";
-echo $header;
+require_once "common.php";
+echo render_header(["title" => "Løping"]);
 ?>
 
 <h1>Inspirasjonsside: Løping</h1>
 
-<img src="../bilder/løping.jpg" alt="Løping i gjørme" >
+<img src="bilder/løping.jpg" alt="Løping i gjørme" >
 
 <p>Løping og jogging er det mange som gjør til daglig. Dette er veldig god trening for hele kroppen. Løping er noe som egner seg for de som har god pykisk styrke.
 De som ikke har psykisk styrke, pleier ikke å presse seg selv godt nok for å få gode og possitve resultater. Løping er noe som passer for de som liker å trene alene,
@@ -20,8 +19,6 @@ er at du kommer ut i trening og kommer inn i en god rytme. Så får du masse ene
 
 <p>Det er også viktig å sette seg et mål, i Oslo så er det mange forskjellige løp du kan melde deg på. <a href="http://www.sentrumslopet.no/">Sentrumsløpet</a></p>
 
-
 <?php
-
 echo $footer;
 ?>
--- www/salsa.php
+++ www/salsa.php
@@ -1,13 +1,10 @@
 <?php
-
-require_once "../common.php";
-
+require_once "common.php";
 echo render_header(["title" => "Salsa"]);
 ?>
 
 <h1>Inspirasjonsside: Salsa</h1>
 
 <?php
-
 echo $footer;
 ?>
