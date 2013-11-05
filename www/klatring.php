4ae70391fdaa5dee9de9ed4f1317b5fbd547e864
Haakon Vangeid <haakon-vangeid@hotmail.com>
2013-11-05 11:21:44 +0100

    lagt til klatring #24

---
+++ www/klatring.php
@@ -0,0 +1,23 @@
+<?php
+require_once "common.php";
+echo render_header(["title" => "klatring"]);
+?>
+
+<img src="bilder/klatring.jpg" alt="klatrevegg">
+
+<p>Klatring er en idrett for deg som har lyst til å utfordre deg seg på flere plan. 
+	Her kommer du opp i høyden og får virkelig testet dine egne grenser høyde messig, men også med tanke på styrke og psyken du innehar i kroppen din. 
+	Klatring er en veldig fysisk idrett som gjør at du får trent veldig mange muskelgrupper og eksplosiviteten i kroppen din </p>
+
+<p>Klatring egner seg for personer som har lyst til å trene på en litt utradisjonell måte, samtidig som man får trent godt og effektivt.
+Klatring er en individuell sport, samtidig som man er sosial med andre som klatrer med deg og det er ofte flere personer som er tilstede samtidig. 
+Dette er noe som kan være bra for veiledning og pushing for nye og bedre resultater.</p>
+
+<p>Studenter i Oslo som vil drive med klatring, kan melde seg inn i: 
+<a href="http://www.osiklatring.no/">OSI klatring</a>.</p>
+
+<?php
+echo $footer;
+?>
+
+
