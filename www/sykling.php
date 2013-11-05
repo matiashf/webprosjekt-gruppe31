cf00f45bee6b8a7d3f50898003a34a3afb5660d5
Sebastian Holmen <sebba_stabak@hotmail.com>
2013-11-05 11:21:59 +0100

    Sykkel siden ferdig (#23)

---
+++ www/sykling.php
@@ -0,0 +1,26 @@
+<?php
+require_once "common.php";
+echo render_header(["title" => "Sykling"]);
+?>
+
+<h1>Inspirasjonsside: Sykling</h1>
+
+<img src="bilder/sykkel.jpg" alt="Sykkler ute i naturen" >
+
+<p>Sykling egner seg for de fleste. Sykling egner seg også veldig bra for de som har kne-skader, eller lignende. Sykling er fin mosjon og er generelt
+	en veldig enkel måte å trene på. Hvis du har en sykkel, så er det bare å hoppe på den og ta seg en tur. Det er også mulig å bruke sykkelen til 
+	å få en lettere/raskere transport ved bruk av egen muskelkraft. Hvis du bruker sykkelen til jobb eller skolen så får du daglig mosjon og det medfører
+	at du kommer i bedre form.</p>
+
+<p>I forhold til trening så finnes det mange forskjellige aktiviteter innenfor sykling. Det er både lag og enslige konkuranser. Landveissykling, banesykling,
+ terrengsykling, banesykling, bmx, er bare noen av mange mulige sykkel aktiviterter. Hvis du bare ønsker å trene så er kanskje spinning noe for deg! Du melder 
+ deg på en time på et treningssenter. Da er det noen som har lagt opp et opplegg, som du kan velge å følge hvis du vil. Her handler det om å presse seg selv og 
+ lide! 
+ </p>
+
+<p>Studenter i Oslo som driver med Sykling , kan melde seg inn i: <a href="http://www.osisykkel.org/">OSI sykkling</a>.</p>
+
+<?php
+echo $footer;
+?>
+
