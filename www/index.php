e4d41795914af05d53acdc2be48ae1952371d720
Matias Hermanrud Fjeld <mhf@hex.no>
2013-11-05 12:09:03 +0100

    La til lenker til inspirasjonssider på forsiden #6

--- www/index.php
+++ www/index.php
@@ -6,7 +6,16 @@ echo render_header(array("title" => "Forsiden"));
 
 ?>
 
-<h1>Hello, world!</h1>
+<h1>Forsiden</h1>
+
+<b>Aktiviteter:</b>
+<ul>
+<li><a href="loping.php">Løping</a></li>
+<li><a href="fotball.php">Fotball</a></li>
+<li><a href="klatring.php">Klatring</a></li>
+<li><a href="salsa.php">Salsa</a></li>
+<li><a href="sykling.php">Sykling</a></li>
+</ul>
 
 <?php
 
