993d313c69962b26f457410f8dfdb231680fe074
Haakon Vangeid <haakon-vangeid@hotmail.com>
2013-11-07 14:12:06 +0100

    endret litt på tennis #35

--- www/tennis.php
+++ www/tennis.php
@@ -7,6 +7,7 @@ echo render_header(array("title" => "Tennis"));
 <img src="bilder/tennis.jpg" alt="tennis">
 
 <p>Tennis er en sport det er lett å lære seg. Har man ikke drevet med Tennis før er det ikke vanskelig å sette seg inn i denne idretten.
+ Hele spillet tennis handler om å få tennisballen over nettet og inn på motstanderens banehalvdel. Den som klarer å spille ut sin motstander på dette får poenget.
 Tennis er en idrett som passer for deg som liker å konkurrere, og drive med en idrett som kun handler om deg selv og dine prestasjoner på banen.  </p>
 
 <p>Tennis er en sport som er veldig fysisk i den grad at man får god kondisjon av å drive med dette. 
