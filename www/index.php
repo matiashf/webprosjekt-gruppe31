ec4f0eb0913403eaae393c578d3ad9bad25bb741
Maria Safdar <s193352@stud.hioa.no>
2013-11-07 14:43:25 +0100

    La til lenke til hiphopsiden #6

--- www/index.php
+++ www/index.php
@@ -57,6 +57,7 @@ bottom, used for guiding liquid or powder into a small opening.» -->
 <li><a href="badminton.php">Badminton</a></li>
 <li><a href="kubb.php">Kubb</a></li>
 <li><a href="tennis.php">Tennis</a></li>
+<li><a href="hiphop.php">Hiphop</a></li>
 </ul>
 </section>
 
