7c7341eb77fbb348260b19532434d8152aa91220
Maria Safdar <s193352@stud.hioa.no>
2013-11-07 15:45:58 +0100

    Inspirasjonsside Basketball #2

---
+++ www/basketball.php
@@ -0,0 +1,24 @@
+<?php
+require_once "common.php";
+echo render_header(array("title" => "Basketball"));
+?>
+
+<h1>Inspirasjonsside: Basketball</h1>
+<!--http://pixabay.com/en/basketball-court-game-sports-women-79599/-->
+<img src="bilder/loping.jpg" alt="Spiller basketball" >
+
+<p>Basketball er en lagsport som spilles mellom to lag. Målet med spillet er å score poeng ved å kaste ballen
+i mostanderens kurv. Ballen kan spilles med hendene ved å kaste den sin sine lagspillere, drible den alene eller slå med uknyttet hånd til motstandere for 
+å få tak i ballen. Dribling er lov hvis ballen stusses i gulvet med en hånd gjentatte ganger.</p>
+
+<p>Det vanlige er at en kamp spilles 4 x 10 min med pauser mellom hver periode. 
+En vanlig scoring gir 2 poeng. Scoring ved straffekast gir 1 poeng mens en scoring fra området utenfor 3-poengslinjen kan også
+gi 3 poeng.</p>
+
+<p>Det finnes forskjellige spilleregler for basketball rundt om i verden. Profesjonell basketball slik som i den amerikanske NBA-ligaen skiller seg f.eks noe fra internasjonal amatørbasketball.</p>
+
+<p>Studenter som har interesse for denne lagsporten kan f.eks melde seg inn i<a href="http://osiwp.bbhoop.net/">OSI Basket</a>.</p>
+
+<?php
+echo $footer;
+?>
