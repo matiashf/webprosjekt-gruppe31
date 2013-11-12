50b8f43433f37ad476ee4aa2e71a96c67953059a
Sebastian Holmen <sebba_stabak@hotmail.com>
2013-11-12 10:38:18 +0100

    Lagt til vollyball og håndball

---
+++ www/handball.php
@@ -0,0 +1,18 @@
+<?php
+require_once "common.php";
+echo render_header(array("title" => "Håndball"));
+?>
+<h1> inspirasjonsside: Håndball </h1>
+
+<!--hhttp://upload.wikimedia.org/wikipedia/commons/a/a6/Nikola_Karabatic_08.jpg-->
+<img src="bilder/handball.jpg" alt="håndball">
+
+<p></p>
+
+<p></p>
+
+<p>Studenter i Oslo som vil drive med håndball kan melde seg inn i <a href="http://www.osi-handball.com/">OSI håndball</a>.</p>
+
+<?php
+echo $footer;
+?>
