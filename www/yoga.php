75c8da6b3bc4c3a1a8e0876e99da39894a8d76a5
Maria Safdar <s193352@stud.hioa.no>
2013-11-14 09:56:47 +0100

    Inspirasjonsside Yoga #46

---
+++ www/yoga.php
@@ -0,0 +1,26 @@
+<?php
+require_once "common.php";
+echo render_header(array("title" => "Yoga"));
+?>
+
+<h1>Inspirasjonsside: Yoga</h1>
+<!--http://commons.wikimedia.org/wiki/File:Yoga_at_a_Gym.JPG/-->
+<img src="bilder/yoga.jpg" alt="Yogaklasse" >
+
+<p>Yoga er en treningsform som fokuserer på både psykisk og fysisk sunnhet. 
+Det finnes ulike varianter av yoga, og i dag er yoga elsket og praktisert av mange, derav også mange kjendiser. 
+Yoga består av ulike avspenningsteknikker der man også fokuserer på å puste kontrollert. 
+</p>
+
+<p>Fordelene med yoga sies å være mange, blant disse er evnen til å stresse mindre, økt bevegelighet, smidighet og
+bedre holdning. 
+(<a href=http://givebackyoga.org/wp-content/uploads/2012/12/The-Health-Benefits-of-Yoga-and-Exercise%EF%80%A2-A-Review-of-Comparison-Studies.pdf/">"The health benefits of Yoga and Exercise"</a></p>
+
+<p>Øvelsene består av fysiske øvelser som kan bestå av både sittende øvelser og stående øvelser. Treningsformen
+oppleves kanskje som lite aktiv for noen, men fordelene sies som sagt å være mange. Og det må da være flere
+grunner til at det er så mange tilhengere av yoga. Har du lyst til å begynne på yoga, så er det flere yoga-kurs
+man kan melde seg på, for eksempel kan du sjekke ut <a href="http://www.yogasenteret.com/">Yogasenteret</a>.</p>
+
+<?php
+echo $footer;
+?>
