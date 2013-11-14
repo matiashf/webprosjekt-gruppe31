750f1d179a15a4318ac755c8453aa65ec6faef0c
Maria Safdar <s193352@stud.hioa.no>
2013-11-14 11:04:02 +0100

    Fikset feil  #47

--- www/pilates.php
+++ www/pilates.php
@@ -1,11 +1,11 @@
 <?php
 require_once "common.php";
-echo render_header(array("title" => "Styrketrening"));
+echo render_header(array("title" => "Pilates"));
 ?>
 
-<h1>Inspirasjonsside: Styrketrening</h1>
-<!--http://www.flickr.com/photos/damndirty/3243399273/-->
-<img src="bilder/styrketrening.jpg" alt="Vektløfting" >
+<h1>Inspirasjonsside: Pilates</h1>
+<!--http://upload.wikimedia.org/wikipedia/commons/9/91/Pilates_01.jpg-->
+<img src="bilder/pilates.jpg" alt="Pilates" >
 
 <p></p>
 
