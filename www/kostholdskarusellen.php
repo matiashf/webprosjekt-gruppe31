6a919b703e4b0780fc7a664a2476cb7c02cdf7ec
Matias Hermanrud Fjeld <mhf@hex.no>
2013-11-14 13:01:20 +0100

    La til tekstinnhold til kostholdskarusellen #7

--- www/kostholdskarusellen.php
+++ www/kostholdskarusellen.php
@@ -7,6 +7,314 @@ echo render_header(array("title" => "Kostholdskarusellen", "header_link" => "<a 
 
 <h1>Kostholdskarusellen</h1>
 
+<article id="variasjon">
+  <h3>Spis variert</h3>
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+
+    <p>Ha et variert kosthold med mye grønnsaker, frukt og
+    bær, grove kornprodukter og fisk, og begrensede mengder bearbeidet
+    kjøtt, rødt kjøtt, salt og sukker.</p>
+
+    <p>Sunn hverdagsmat og et variert kosthold gir deg et godt grunnlag
+    for god helse, og bidrar til at du får i deg næringsstoffene du
+    trenger. Ha et variert kosthold med mye grønnsaker, frukt og bær,
+    grove kornprodukter og fisk, og begrensede mengder bearbeidet kjøtt,
+    rødt kjøtt, salt og sukker.</p>
+
+    <ul>
+
+      <li>Velg matvarer med begrensede mengder mettet fett, sukker og
+      salt.</li>
+
+      <li>Se etter Nøkkelhullet når du handler. Det gjør det enklere å sette
+      sammen et sunt, variert og godt kosthold.</li>
+
+    </ul>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Spis-variert.aspx">Mer
+    om variert kosthold (helsenorge.no)</a></p>
+
+  </blockquote>
+</article>
+
+<article id="energibalanse">
+  <h3>Spis i forhold til aktivitetsnivå</h3>
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+
+    <p>Ha en god balanse mellom hvor mye energi du får i deg gjennom mat
+    og drikke, og hvor mye du forbruker gjennom aktivitet.</p>
+
+    <p>Ved å opprettholde normalvekten din reduserer du også risikoen for
+    helseproblemer. Om du ønsker å gå ned i vekt, er det særlig viktig at
+    du har fokus på hva og hvor mye du spiser i forhold til hvor fysisk
+    aktiv du er.</p>
+
+    <ul>
+
+      <li>Begrens inntaket av mat og drikke som har høyt energiinnhold og
+      lite næring, som brus, godteri og snacks. </li>
+
+      <li>Regelmessig fysisk aktivitet bidrar til å opprettholde kroppens
+      energibalanse.</li></ul>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Hold-energibalansen.aspx">Mer
+    om energibalanse (helsenorge.no)</a></p>
+
+  </blockquote>
+</article>
+
+<article id="fem-om-dagen">
+  <h3>Spis frukt og grønnsaker</h3>
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+
+    <p>Spis minst fem porsjoner grønnsaker, frukt og bær hver dag. La
+    grønnsaker og frukt inngå i alle dagens måltider. Gå på
+    oppdagelsesferd i frukt- og grøntavdelingen. Smak og
+    eksperimenter. Velg ulike farger.</p>
+
+    <ul>
+
+      <li>En porsjon tilsvarer 100 gram, og kan for eksempel være en liten
+      bolle med salat, en gulrot eller en middels stor frukt.</li>
+
+      <li>Du kan bruke friske, hermetiske, frosne, og varmebehandlede
+      grønnsaker, frukt og bær. Mulighetene er mange. Halvparten av «fem
+      om dagen» bør være grønnsaker.</li>
+
+      <li>Varier mellom ulike typer grønnsaker og frukt. Bruk gjerne
+      tomater, løk,purre og hvitløk. </li>
+
+      <li>Poteter er ikke inkludert i «fem om dagen», men hører med i et
+      variert kosthold. Poteter inneholder mer kostfiber, vitaminer og
+      mineraler enn vanlig ris og pasta. Du bør helst velge kokte eller
+      bakte poteter. </li>
+
+      <li>Belgvekster som bønner og linser, frø, krydder og urter inngår
+      heller ikke i «fem om dagen». Disse har ofte et høyt innhold av
+      næringsstoffer og hører med i et variert kosthold.</li>
+
+      <li>Spis gjerne en liten håndfull usaltede nøtter om
+      dagen.</li>
+    </ul>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Spis-fem-om-dagen.aspx">Mer
+    om grønnsaker, frukt og bær (helsenorge.no)</a></p>
+
+  </blockquote>
+</article>
+
+<article id="grove-kornprodukter">
+  <h3>Spis grove kornprodukter hver dag</h3>
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+
+    <p>Spis grove kornprodukter hver dag. For mange nordmenn er grovt brød
+    en selvfølgelig del av hverdagen. Det er bra fordi grove kornprodukter
+    er sunnere enn fine kornprodukter.</p>
+
+    <ul>
+      <li>Velg kornprodukter med høyt innhold av fiber og fullkorn, og lavt
+      innhold av fett, sukker og salt. Bruk Nøkkelhullet og Brødskala’n som
+      hjelpemidler.</li>
+
+      <li>De grove kornproduktene bør til sammen gi 70-90 gram sammalt mel
+      eller fullkorn per dag.</li>
+
+      <li>
+        Her er fire eksempler på hvordan denne mengden kan dekkes:
+        <ul>
+          <li>fire brødskiver med en stor andel sammalt mel, for eksempel merket ekstra grovt i Brødskala’n.</li>
+          <li>én tallerken grov kornblanding og to skiver ekstra grovt brød.</li>
+          <li>én tallerken havregrøt og én porsjon fullkornspasta eller fullkornsris.</li>
+          <li>to tallerkener havregrøt.</li>
+        </ul>
+      </li>
+    </ul>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Spis-grove-kornprodukter-hver-dag.aspx">Mer
+    om grove kornprodukter (helsenorge.no)</a></p>
+
+  </blockquote>
+</article>
+
+<article id="fisk">
+  <h3>Spis fisk</h3>
+
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+    <p>Spis fisk til middag to til tre ganger i uken. Bruk også gjerne
+    fisk som pålegg. Fisk er en fantastisk råvare som kan brukes til
+    mye. Eksperimenter med nye måter å tilberede og servere fisken
+    på.</p>
+
+    <ul>
+      <li>Rådet tilsvarer totalt 300-450 gram ren fisk i uken.</li>
+      <li>Minst 200 gram bør være fet fisk som laks, ørret, makrell
+      eller sild.</li>
+      <li>Seks påleggsporsjoner med fisk tilsvarer omtrent én
+      middagsporsjon.</li>
+      <li>Velg gjerne fiskeprodukter merket med Nøkkelhullet.</li>
+    </ul>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Spis-fisk-oftere.aspx">Mer
+    om fisk (helsenorge.no)</a></p>
+  </blockquote>
+</article>
+
+<article id="magert-kjott">
+  <h3>Spis magert kjøtt</h3>
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+
+    <p>Velg magert kjøtt og magre kjøttprodukter. Begrens mengden bearbeidet kjøtt og rødt kjøtt.</p>
+
+    <p>Det er lurt å tenke over hva slags kjøtt du velger, hvor mye og
+    hvor ofte du spiser det. Rene råvarer er et bedre valg enn
+    bearbeidet kjøtt.</p>
+
+    <ul>
+      <li>Velg hvitt kjøtt, rent kjøtt og magre kjøttprodukter med
+      lite salt.</li>
+
+      <li>Begrens mengden av bearbeidede kjøttprodukter som er
+      røkt, saltet eller konservert med nitrat eller nitritt, som
+      for eksempel bacon eller spekepølse.</li>
+
+      <li>Begrens mengden rødt kjøtt og bearbeidede produkter av rødt
+      kjøtt til 500 gram per uke. Dette tilsvarer to til tre middager
+      og litt kjøttpålegg. Rødt kjøtt er kjøtt fra svin, storfe, sau
+      og geit.</li>
+
+      <li>Velg fortrinnsvis nøkkelhullsmerket kjøtt og
+      kjøttprodukter.</li>
+    </ul>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Velg-magert-kjott.aspx">Mer
+    om kjøtt og kjøttprodukter (helsenorge.no)</a></p>
+  </blockquote>
+</p>
+
+<article id="magre-meieriprodukter">
+  <h3>Spis magre meieriprodukter</h3>
+
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+    <p>La magre meieriprodukter være en del av det daglige
+    kostholdet. Melk og ost er en del av hverdagsmaten for mange. Det er
+    bra. Husk at det er lurt å velge de magre meieriproduktene til
+    hverdags.</p>
+
+    <ul>
+      <li>Begrens bruken av meieriprodukter med mye mettet fett, som
+      helmelk, fløte, fet ost og smør. Velg meieriprodukter med lite fett,
+      salt og lite tilsatt sukker. </li>
+
+      <li>Velg fortrinnsvis meieriprodukter merket med
+      Nøkkelhullet.</li>
+    </ul>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Velg-magre-meieriprodukter.aspx">Mer
+    om magre meieriprodukter (helsenorge.no)</a></p>
+  </blockquote>
+</article>
+
+<article id="fett">
+  <h3>Spis sunt fett</h3>
+
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+        <p>Velg matoljer, flytende margarin og myk margarin, fremfor hard margarin og smør.</p>
+
+    <p>Det er viktig å sikre en god fettsyresammensetning i
+    kostholdet. Bytt ut mettede fettsyrer med mer gunstige umettede
+    fettsyrer. En tommelfingerregel er at jo mykere margarinen og
+    smøret er ved kjøleskapstemperatur, desto mer umettet fett
+    inneholder de.</p>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Velg-matoljer-og-myk-margarin.aspx">Mer
+    om matoljer og margarin (helsenorge.no)</a> </p>
+  </blockquote>
+</article>
+
+<article id="salt">
+  <h3>Spis lite salt</h3>
+
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+    <p>Velg matvarer med lite salt, og begrens bruken av salt i
+    matlagning og på maten.</p>
+
+    <p>Ønsker du å sette mer smak på maten du lager? Bruk krydder og
+    urter for å gi maten personlig smak uten å tilsette ekstra
+    salt.</p>
+
+    <ul>
+      <li>Bearbeidede matvarer bidrar i snitt med 70-80 prosent av
+      saltinntaket.</li>
+
+      <li>Velg derfor matvarer og ferdigretter med lavt
+      saltinnhold.</li>
+
+      <li>Se etter nøkkelhullsmerkede matvarer og retter når du
+      handler.</li>
+    </ul>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Spis-mindre-salt.aspx">Mer
+    om salt (helsenorge.no)</a></p>
+  </blockquote>
+</article>
+
+<article id="sukker">
+  <h3>Spis lite sukker</h3>
+
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+    <p>Unngå mat og drikke med mye sukker til hverdags.</p>
+
+    <p>Brus, saft og godteri er de største kildene til tilsatt sukker
+    i kosten. De tilfører mye sukker og energi, men lite vitaminer og
+    mineraler.</p>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Spis-og-drikk-mindre-sukker-til-hverdags.aspx">Mer
+    om sukker (helsenorge.no)</a></p>
+  </blockquote>
+</article>
+
+<article id="vann">
+  <h3>Drikk vann</h3>
+
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+    <p>Velg vann som tørstedrikk.</p>
+
+    <p>Vann er nødvendig for å opprettholde normale
+    kroppsfunksjoner. Vanlig vann dekker væskebehovet uten å bidra med
+    unødvendige kalorier, og er derfor den aller beste drikken når du
+    er tørst.</p>
+
+    <p><a href="http://helsenorge.no/Helseogsunnhet/Sider/Velg-vann-som-torstedrikk.aspx">Mer
+    om vann som tørstedrikk (helsenorge.no)</a></p
+  </blockquote>
+</article>
+
+<article id="aktivitet">
+  <h3>Hold vekten med aktivitet</h3>
+
+  <blockquote cite="http://helsedirektoratet.no/folkehelse/ernering/kostholdsrad/Sider/default.aspx">
+    <p>Vær fysisk aktiv i minst 30 minutter hver dag.</p>
+
+    <p>All bevegelse er bra både for kropp og sjel. Små grep i
+    hverdagen kan gjøre stor forskjell for helsen. Ta for eksempel
+    trappene i stedet for heisen, gå en kveldstur eller dra i
+    svømmehallen.</p>
+
+    <ul>
+      <li>Bruk minst 30 minutter hver dag til fysisk aktivitet
+      tilsvarende hurtig gange.</li>
+
+      <li>Del gjerne aktiviteten opp i bolker i løpet av
+      dagen. Fysisk aktivitet er gunstig ved vektreduksjon og for
+      forebygging av vektøkning. </li>
+
+      <li>For å vedlikeholde vekten etter vektreduksjon anbefales
+      60-90 minutter moderat fysisk aktivitet de fleste dager i
+      uken.</li>
+    </ul>
+  </blockquote>
+</article>
+
 <?php
 
 echo $footer;
