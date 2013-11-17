f30a5faa6b61947635664372062623a0b1bea6ef
Sebastian Holmen <sebba_stabak@hotmail.com>
2013-11-17 12:40:27 +0100

    Skrevet om håndball!

--- www/handball.php
+++ www/handball.php
@@ -7,9 +7,12 @@ echo render_header(array("title" => "Håndball"));
 <!--hhttp://upload.wikimedia.org/wikipedia/commons/a/a6/Nikola_Karabatic_08.jpg-->
 <img src="bilder/handball.jpg" alt="håndball">
 
-<p></p>
+<p>Håndball er en lag idrett, dette er en sport som man spiller innendørs. Håndball er en ballsport, 
+målet er å score på motstanderens sitt mål, og unngå baklengs mål. Den som scorer mest mål har vunnet kampen. </p>
 
-<p></p>
+<p>Håndball egner seg for de som liker å samarbeide med andre og sosialt samvær. Håndball er også en fysisk sport, 
+så du må tåle å bli tatt på. Håndball ser ikke så krevende ut, men det er veldig slitsomt når du spiller. På mange måter 
+så blir hådball en intervall sport. Du kommer fort i form, og det er i tillegg kjempegøy! </p>
 
 <p>Studenter i Oslo som vil drive med håndball kan melde seg inn i <a href="http://www.osi-handball.com/">OSI håndball</a>.</p>
 
