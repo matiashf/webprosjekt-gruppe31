f7cb22e4cf6387f709092516b81e88b99f8e671b
Maria Safdar <s193352@stud.hioa.no>
2013-11-17 19:37:47 +0100

    Skrevet ferdig tekst #22

--- www/styrketrening.php
+++ www/styrketrening.php
@@ -7,13 +7,20 @@ echo render_header(array("title" => "Styrketrening"));
 <!--http://www.flickr.com/photos/damndirty/3243399273/-->
 <img src="bilder/styrketrening.jpg" alt="Vektløfting" >
 
-<p></p>
+<p>Styrketrening er trening med mål å øke muskelmassen og få en sterkere og mer markert kropp.</p>
 
-<p></p>
+<p>Styrketrening egner seg for alle som vil bli sterkere. Men folk som ikke er så kjent med trening
+burde ta det litt med ro og eventuelt spørre en personlig trener eller rådføre seg med noen som har peil
+før de begynner å trene for hardt. I styrketrening er det essensielt med riktig løfteteknikk
+og feil løfteteknikk kan gi belastningsskader. Det er viktig å starte smått og kroppen bør helst
+bli varm før man begynner å løfte. </p>
 
-<p></p>
+<p>Man kan trene styrke med egen kroppsvekt, manualer eller spesielle apparater for å trene styrke.
+Man kan trene hjemme, ute i det fri eller på treningsstudio der de ofte har flere ulike apparater for kroppens
+ulike muskelgrupper.</p>
 
-<p></p>
+<p>Er du interessert i å begynne å trene kan du for eksempel melde deg inn i 
+<a href="http://www.sio.no/wps/portal/sio/idrett?reset=true">Sio Trening</a>.</p>
 
 <?php
 echo $footer;
