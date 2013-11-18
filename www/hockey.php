2330393c89af9b62f83e3f37f1ae380203fa163e
Maria Safdar <s193352@stud.hioa.no>
2013-11-18 03:36:33 +0100

    Ferdig med hockey #53

---
+++ www/hockey.php
@@ -0,0 +1,25 @@
+<?php
+require_once "common.php";
+echo render_header(array("title" => "Hockey"));
+?>
+
+<h1>Inspirasjonsside: Hockey</h1>
+<!--http://upload.wikimedia.org/wikipedia/commons/4/4b/Ice_Hockey_sharks_ducks.jpg-->
+<img src="bilder/hockey.jpg" alt="Ishockey" >
+
+<p>Hockey er et samlebegrep for idretter som har til felles at spillerne forsøker å 
+føre en ball eller puck inn i motstanderens mål ved hjelp av en kølle. Det finnes i hovedsak to varianter
+for hockey som er ishockey (bildet) og landhockey.</p>
+
+<p>Idretten kan minne litt om bandy, og passer for de som liker å spille i lag. Det er tøff trening, men 
+spillerne minsker faren for idrettskader ved å bruke hjelm og beskyttelse.
+Her kan man også tenke over hva man liker best, å spille på is eller land.</p>
+
+<p>Fikk du lyst til å være med i en ishockeyklubb kan du sjekke  
+<a href="http://www.aktivioslo.no/ishockey/">Oversikt over ishockeyforbund i Oslo/Akershus</a>.
+Hvis du synes landhockey og innebandy hørtes bedre ut kan du sjekke
+<a href="http://www.aktivioslo.no/landhockey/">Landhockeyklubber og innebandyklubber i Oslo/Akershus.</p>
+
+<?php
+echo $footer;
+?>
