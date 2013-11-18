f980f1619e16eca6e31acef839af4920f0bad4f2
Maria Safdar <s193352@stud.hioa.no>
2013-11-18 02:57:47 +0100

    Ferdig med boksing #52

---
+++ www/boksing.php
@@ -0,0 +1,24 @@
+<?php
+require_once "common.php";
+echo render_header(array("title" => "Boksing"));
+?>
+
+<h1>Inspirasjonsside: Boksing</h1>
+<!--http://upload.wikimedia.org/wikipedia/commons/f/ff/Boxing_%28USA_vs._CAN%29_at_the_1995_Pan_American_Games.JPEG/-->
+<img src="bilder/boksing.jpg" alt="Boksingmatch" >
+
+<p>Boksing er en kamp som spilles mellom to personer med polstrede lærhansker på hendene.
+Man kan vinne ved å slå motstanderen flest ganger, noe man blir belønnet med poeng på
+eller så kan man vinne på knockout. Knockout vil si at man slår sin motstander ned eller kampudyktig 
+på annen måte i minst 10 sekunder.</p>
+
+<p>Kampen foregår i en boksering med begrenset område som består av taulinje rundt. 
+Boksing er tøff trening som trener både styrke og kondisjon. Det passer for de som liker trening med 
+høy intensitet.</p>
+
+<p>Tror du boksing er noe for deg kan du jo sjekke ut 
+<a href="http://osi-boksing.com/">OSI Boksing</a>.</p>
+
+<?php
+echo $footer;
+?>
