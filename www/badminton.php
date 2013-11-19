93d4758573549411e1dabbb1cd0e3ad6f98e42b3
Sebastian Holmen <sebba_stabak@hotmail.com>
2013-11-19 10:38:52 +0100

    test badminton (#38)

--- www/badminton.php
+++ www/badminton.php
@@ -6,7 +6,7 @@ echo render_header(array("title" => "Badminton"));
 
 <h1>Inspirasjonsside: Badminton </h1>
 
-<img optimized_image()-kall="bilder/badminton.jpg" alt="Badminton" >
+<?php optimized_image(array("src" => "bilder/badminton.jpg", "alt" => "")); ?>
 
 <p>Badminton er en sport som er for 2-4 spillere. Det handler om å få fjærballen over nettet, sånn at man ikke taper. Hvis fjærballen lander på gulvet på din side har du tapt. Denne sporten egner seg for de fleste, det eneste som du bør "ha" er gode reflekser. Fjærballen går fort fram og tilbake så gode reflekser kan være
 en god fordel. </p>
