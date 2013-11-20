be0f47cdb77632d959a58ad5c3653f8de50b7821
Sebastian Holmen <sebba_stabak@hotmail.com>
2013-11-20 12:34:23 +0100

    endret bildekode (#38 #29)

--- www/kubb.php
+++ www/kubb.php
@@ -5,7 +5,7 @@ echo render_header(array("title" => "kubb"));
 <h1>Inspirasjonsside: Kubb</h1>
 
 <!-- http://pixabay.com/no/park-skog-treet-natur-landskap-191603/-->
-<img src="bilder/kubb.jpg" alt="kubb">
+<?php optimized_image(array("src" => "bilder/kubb.jpg", "alt=kubb" => "")); ?>
 
 <blockquote lang="en">
 
