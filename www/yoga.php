b0b8263a9376089f96d7124e6c142ebdf4083420
Maria Safdar <s193352@stud.hioa.no>
2013-11-24 14:01:44 +0100

    endret link #26

--- www/yoga.php
+++ www/yoga.php
@@ -22,7 +22,7 @@ Yoga består av ulike avspenningsteknikker der man også fokuserer på å puste 
 
 <p>Fordelene med yoga sies å være mange, blant disse er evnen til å stresse mindre, økt bevegelighet, smidighet og
 bedre holdning. 
-(<a href=http://givebackyoga.org/wp-content/uploads/2012/12/The-Health-Benefits-of-Yoga-and-Exercise%EF%80%A2-A-Review-of-Comparison-Studies.pdf/">"The health benefits of Yoga and Exercise)</a></p>
+<br><a href="http://givebackyoga.org/wp-content/uploads/2012/12/The-Health-Benefits-of-Yoga-and-Exercise%EF%80%A2-A-Review-of-Comparison-Studies.pdf/">(The health benefits of Yoga and Exercise)</a></p>
 
 <p>Øvelsene består av fysiske øvelser som kan bestå av både sittende øvelser og stående øvelser. Treningsformen
 oppleves kanskje som lite aktiv for noen, men fordelene sies som sagt å være mange. Og det må da være flere
