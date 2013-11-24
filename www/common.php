a79f0d7781bb3d995f93c5f718b47935305db6ce
Matias Hermanrud Fjeld <mhf@hex.no>
2013-11-24 22:18:23 +0100

    Endret navn på twitter-lenke for tekstbrowsere

--- www/common.php
+++ www/common.php
@@ -121,7 +121,7 @@ ob_start();
 
       <!-- https://about.twitter.com/resources/buttons#tweet -->
       <div class="share-to-twitter">
-        <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+        <a href="https://twitter.com/share" class="twitter-share-button">Del på Twitter</a>
         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
       </div>
 
